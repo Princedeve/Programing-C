88f7a95407e5b81058455ed1a756abb2d43a17fb
Prince <prince935059@gmail.com>
2025-10-15 18:17:12 +0530

    update code

--- pointer/practic.c
+++ pointer/practic.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-    // 2 Write a program having a variable i. Print the address of i. Pass this variable to a function and print its address. Are these addresses same? Why?
     int returning_5(int* ptr){
         printf("The value of ptr is %d\n", ptr);
         printf("The value at ptr is %d\n", *ptr);
         return 5;
+    }
+
+    void change_to_ten_times(int* a){
+        *a = *a * 10;
     }
 int main(){
     // 1 Write a program to print the address of a variable. Use this address to get the value of the variable.
@@ -12,9 +15,14 @@ int main(){
     printf("The address of a is %p\n", &a);
     printf("The address of a is %u\n", &a);
     printf("The value of a is %d\n", *ptr);
+    // 2 Write a program having a variable i. Print the address of i. Pass this variable to a function and print its address. Are these addresses same? Why?
     returning_5(ptr);
 
     // 3 Write a program to change the value of a variable to ten times of its current value.
+    int x = 45;
+    printf("The value of x is %d\n", x);
+    change_to_ten_times(&x);
+    printf("The value of x is %d\n", x);
     // 4 Write a function and pass the value by reference.
     // 5 Write a program using a function which calculates the sum and average of two numbers. Use pointers and print the values of sum and average of two numbers. Use pointers and print the values of sum and average in main().
     // 6 Write a program to print the value of a variable i by using "pointer to pointer" type of variable.
