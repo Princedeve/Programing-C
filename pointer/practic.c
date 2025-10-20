965b0a6dfb24ccbadcb202408188dbff794eb9b3
Prince <prince935059@gmail.com>
2025-10-20 18:49:18 +0530

    update file code

--- pointer/practic.c
+++ pointer/practic.c
@@ -54,8 +54,11 @@ int main(){
     printf("The address of sum is %u and average is %u\n", ptr1, ptr2);
     // 6 Write a program to print the value of a variable i by using "pointer to pointer" type of variable.
     int v = 24;
-    int* p = &v;
-    printf("%d", p);
+    int *v1 = &v;
+    int **v2 = &v1;
+    printf("The address of v is %u\n", &v);
+    printf("The address of v is %d\n", *v1);
+    printf("The address of v is %d\n", **v2);
     // 7 Try problem 3 using call by value and verify that it does not change the value of the said variable.
 
     return 0;
