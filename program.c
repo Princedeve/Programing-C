32ee5cc4944689beac0ce870f3c7446d8167a40b
Prince <prince935059@gmail.com>
2025-12-14 16:37:33 +0530

    update code

---
+++ program.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int squre(int n){
+    return (n*n);
+}
+
+int main(){
+    // int i = 1;
+    // while (i <= 10)
+    // {
+    //     printf("%d", i);
+    //     i++;
+    // }
+
+    int num = 3;
+    printf("%d", squre(num));
+    
+    return 0;
+}
