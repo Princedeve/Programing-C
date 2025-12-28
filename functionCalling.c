701964f175883986c6a5ec8edabc44ba1c3d6a7e
Prince <prince935059@gmail.com>
2025-12-28 20:37:15 +0530

    add file

---
+++ functionCalling.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void m3(){
+printf("inside m3 function\n");
+}
+void m2(){
+m3();
+printf("inside m2 function\n");
+}
+void m1(){
+m2();
+printf("inside m1 function\n");
+}
+int main(){
+    printf("Execution started... from main function\n");
+    m1();
+    printf("Execution ended... from the main function\n");
+    return 0;
+}
